19d51946ce7099b9f3c40de331917ec25adefd51
swerly <seth.werly@gmail.com>
2017-07-05 23:02:04 -0700

    Minor improvements, still starting.
    Started adding actionbar functionality for different activities/fragments to be used.
    Started linking activities together to create basic app flow.

--- app/src/main/java/com/swerly/wifiheatmap/BaseActivity.java
+++ app/src/main/java/com/swerly/wifiheatmap/BaseActivity.java
@@ -1,10 +1,17 @@
 package com.swerly.wifiheatmap;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by Seth on 7/3/2017.
+ *
+ * Base Activity that all other activities will inherit from
  */
 
 public class BaseActivity extends AppCompatActivity {
@@ -14,5 +21,46 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         app = (BaseApplication) getApplication();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_menu, menu);
+
+        int menLen = menu.size();
+        for(int i = 0; i < menLen; i++){
+            MenuItem item = menu.getItem(i);
+            Drawable drawable = item.getIcon();
+            if (drawable != null) {
+                final Drawable wrapped = DrawableCompat.wrap(drawable);
+                drawable.mutate();
+                DrawableCompat.setTint(wrapped, getResources().getColor(R.color.white));
+                item.setIcon(drawable);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_undo:
+                break;
+            case R.id.action_redo:
+                break;
+            case R.id.action_filter:
+                break;
+            case R.id.action_search:
+                break;
+            case R.id.action_help:
+                break;
+            case R.id.action_location:
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/swerly/wifiheatmap/HomeActivity.java
+++ app/src/main/java/com/swerly/wifiheatmap/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.swerly.wifiheatmap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends BaseActivity {
 
@@ -8,5 +10,18 @@ public class HomeActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fabSetup();
     }
+
+    private void fabSetup(){
+        findViewById(R.id.home_fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeActivity.this, FunctionalityActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
 }
